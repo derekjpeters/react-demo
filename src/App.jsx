f8d7cdc339e4e9295cb3ef275064769729f275c8
derekjpeters <derek.j.peters@gmail.com>
2025-09-11 19:26:27 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import LifeCycleDemo from "./components/LifecycleDemo";
+import Clock from "./components/Clock";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App () {
+
+  const [show, setShow] = useState(true);
+  const [title, setTitle] = useState("LifeCycleDemo");
+  const [inputTitle, setInputTitle] = useState(title);
+
+  useEffect(() => {
+    console.log("App - mounted")
+    return () => console.log("App - unmounted")
+  }, []);
+
+  useEffect(() => {
+    console.log("App - title changed", title)
+  }, [title]);
+
+  const toggle = () => {
+    console.log("App toggle show", {from: show, to: !show})
+    setShow((s) => !s);
+  };
+
+  const bumpTitle = () => {
+    console.log("App - bumpTitle")
+    setTitle((t) => t + " -")
+  }
+
+  const applyInputTitle = (e) => {
+    e.preventDefault();
+    console.log("App - apply input title", inputTitle);
+    setTitle(inputTitle)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>React Lifecycle (Class Components) </h1>
 
-export default App
+      <div>
+        <button onClick={toggle}>{show ? "Unmount Demo": "Mount Demo"}</button>
+        <button onClick={bumpTitle}>Update Prop: Title (append -)</button>
+      </div>
+
+      <form onSubmit={applyInputTitle}>
+        <label>
+          Edit Title: {" "}
+          <input 
+          value = {title}
+          onChange={(e) => {
+            console.log("App - input change:", e.target.value);
+            setInputTitle(e.target.value);
+          }}
+          />
+        </label>
+        <button>
+          Apply
+        </button>
+        {show && (
+          <>
+            <LifeCycleDemo title = {title} />
+            <Clock/> {/*New */}
+          </>
+        )}
+      </form>
+    </div>
+  )
+
+}
